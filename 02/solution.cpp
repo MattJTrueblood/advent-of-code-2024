5e752ddbf29dbfba0c1055a7043efbd43735497b
MattJTrueblood <mattjtrueblood@gmail.com>
2024-12-11 18:21:38 +0100

    adding 02 solution part 2

--- 02/solution.cpp
+++ 02/solution.cpp
@@ -1,7 +1,7 @@
 
-/**********************
- * Day N:  ~~~~~~~~~  *
- **********************/
+/*****************************
+ * Day 2: Red-Nosed Reports  *
+ *****************************/
 
 #include <iostream>
 #include <fstream>
@@ -11,11 +11,71 @@
 
 using namespace std;
 
-static const bool PART_2_ENABLED = false;
+static const bool PART_2_ENABLED = true;
+
+vector<vector<string>> getSkipOneVariationsOfLine(vector<string> line) {
+    vector<vector<string>> result;
+
+    for (int i = 0; i < line.size(); i++) {
+        vector<string> variation = line;
+        variation.erase(variation.begin() + i);
+        result.push_back(variation);
+    }
+
+    return result;
+}
+
+bool lineIsSafe(vector<string> line) {
+    int previous = -1;
+    bool descending;
+    for(int i = 0; i < line.size(); i++) {
+        int current = stoi(line[i]);
+        if(i > 0) {
+            //check if diff between 1 and 3
+            int diff = abs(current - previous);
+            if(diff < 1 || diff > 3) {
+                return false;
+            }
+
+            if(i == 1) {
+                // figure out ascending or descending
+                if(previous < current) {
+                    descending = false;
+                } else {
+                    descending = true;
+                }
+            }
+            if(i > 1) {
+                if(descending == true) {
+                    if(previous < current) {
+                        return false;
+                    }
+                }
+                if(descending == false) {
+                    if(previous > current) {
+                        return false;
+                    }
+                }
+            }
+        }
+        previous = current;
+    }
+    return true;
+}
 
 long long part2(vector<string> lines) {
-    // TODO
-    return -1;
+    vector<vector<string>> wordsInLines = parseAllLines(lines);
+    long long safeCount = 0;
+    for(vector<string> line: wordsInLines) {
+        vector<vector<string>> variations = getSkipOneVariationsOfLine(line);
+        for(vector<string> variation: variations) {
+            if(lineIsSafe(variation)) {
+                safeCount++;
+                break;
+            }
+        }
+    }
+    return safeCount;
 }
 
 long long part1(vector<string> lines) {
@@ -23,46 +83,7 @@ long long part1(vector<string> lines) {
     long long safeCount = 0;
 
     for(vector<string> line: wordsInLines) {
-        int previous = -1;
-        bool descending;
-        bool safe = true;
-        
-        for(int i = 0; i < line.size(); i++) {
-            int current = stoi(line[i]);
-            if(i > 0) {
-                //check if diff between 1 and 3
-                int diff = abs(current - previous);
-                if(diff < 1 || diff > 3) {
-                    safe = false;
-                    break;
-                }
-
-                if(i == 1) {
-                    // figure out ascending or descending
-                    if(previous < current) {
-                        descending = false;
-                    } else {
-                        descending = true;
-                    }
-                }
-                if(i > 1) {
-                    if(descending == true) {
-                        if(previous < current) {
-                            safe = false;
-                            break;
-                        }
-                    }
-                    if(descending == false) {
-                        if(previous > current) {
-                            safe = false;
-                            break;
-                        }
-                    }
-                }
-            }
-            previous = current;
-        }
-        if(safe == true) {
+        if(lineIsSafe(line)) {
             safeCount++;
         }
     }
